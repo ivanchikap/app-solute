8ea3c21edaafa705b4594b9ae4e8b33748570827
ivanchikap <anton.ivanchik@gmail.com>
2019-11-06 12:08:45 +0300

    part5 27m

---
+++ src/js/init.js
@@ -0,0 +1,26 @@
+$('.screenshots__carousel').slick({
+    centerMode: true,
+    centerPadding: '140px',
+    slidesToShow: 3,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
